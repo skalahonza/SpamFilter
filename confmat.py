c555a981458f596d9dadd38bb6016a7eb61ada45
Jan Skala <j.skala@hotmail.com>
2016-12-12 13:35:06 +0100

    Basic tests pass

--- confmat.py
+++ confmat.py
@@ -10,12 +10,15 @@ class BinaryConfusionMatrix:
     def as_dict(self):
         return {"tp": self.TP, "tn": self.TN, "fp": self.FP, "fn": self.FN}
 
-    def eval_tag(self, tag):
-        return tag == self.pos_tag or tag == self.neg_tag
+    def wrong_tag(self, tag):
+        if tag == self.pos_tag or tag == self.neg_tag:
+            return False
+        return True
+
 
     def update(self, truth, prediction):
-        if not self.eval_tag(truth) or not self.eval_tag(prediction):
-            exec(ValueError)
+        if self.wrong_tag(truth) or self.wrong_tag(prediction):
+            raise ValueError
         else:
             # TRUE POSITIVE
             if prediction == self.pos_tag and truth == self.pos_tag:
@@ -28,9 +31,24 @@ class BinaryConfusionMatrix:
                 self.FP += 1
             # FALSE NEGATIVE
             elif prediction == self.neg_tag and truth == self.pos_tag:
-                self.FP += 1
+                self.FN += 1
 
     def compute_from_dicts(self, truth_dict, prediction_dict):
-        for key, value in truth_dict:
-            pass
-        pass
+        for key, value in truth_dict.items():
+            predicted = prediction_dict[key]
+            self.update(value, predicted)
+
+
+if __name__ == "__main__":
+    cm1 = BinaryConfusionMatrix(pos_tag=True, neg_tag=False)
+    print(cm1.as_dict())
+    # {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
+    cm1.update(True, True)
+    print(cm1.as_dict())
+    # {'tp': 1, 'tn': 0, 'fp': 0, 'fn': 0}
+    truth_dict = {'em1': 'SPAM', 'em2': 'SPAM', 'em3': 'OK', 'em4': 'OK'}
+    pred_dict = {'em1': 'SPAM', 'em2': 'OK', 'em3': 'OK', 'em4': 'SPAM'}
+    cm2 = BinaryConfusionMatrix(pos_tag='SPAM', neg_tag='OK')
+    cm2.compute_from_dicts(truth_dict, pred_dict)
+    print(cm2.as_dict())
+    #  {'tp': 1, 'tn': 1, 'fp': 1, 'fn': 1}
